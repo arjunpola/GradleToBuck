f2418d70d98e248c15dc97b810a1f8093a1fad6d
Nagarjun Pola <nagarjun.pola@ticketmaster.com>
2016-03-05 23:03:03 -0800

    Initial Commit

---
+++ app/src/main/java/com/example/gradletobuck/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.gradletobuck;
+
+import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
